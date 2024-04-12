999a3e72152def184a97dda75bfaa86b6b228b7a
edsoncuno1@gmail.com <edsoncuno1@gmail.com>
2024-04-12 20:31:57 +0000

    los iconos estaban ya en la carpeta icons y no me di cuenta

--- src/pages/home/about-me/data.js
+++ src/pages/home/about-me/data.js
@@ -1,38 +1,38 @@
 const data = {
   tools: [
     {
-      url: "/tools/microsoft-office.svg",
+      url: "/icons/microsoft-office.svg",
       name: "Microsoft Office",
       shadowColor: "#eb3c00",
     },
     {
-      url: "/tools/power-bi.svg",
+      url: "/icons/power-bi.svg",
       name: "Power BI",
       shadowColor: "#e3ad11",
     },
 
     {
-      url: "/tools/visual-studio-code.svg",
+      url: "/icons/visual-studio-code.svg",
       name: "Visual Studio Code",
       shadowColor: "#0179cb",
     },
     {
-      url: "/tools/microsoft-windows.svg",
+      url: "/icons/microsoft-windows.svg",
       name: "Windows",
       shadowColor: "#00adef",
     },
     {
-      url: "/tools/ubuntu.svg",
+      url: "/icons/ubuntu.svg",
       name: "Ubuntu",
       shadowColor: "#e95420",
     },
     {
-      url: "/tools/archlinux.svg",
+      url: "/icons/archlinux.svg",
       name: "Arch Linux",
       shadowColor: "#1793d1",
     },
     {
-      url: "/tools/git.svg",
+      url: "/icons/git.svg",
       name: "Git",
       shadowColor: "#ee513b",
     },
